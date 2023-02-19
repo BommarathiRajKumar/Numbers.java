2ad9175a134755c3e53130746cecfc99287e54ad
RajKumar Bommarathi <90762085+BommarathiRajKumar@users.noreply.github.com>
2023-02-19 12:09:59 +0530

    Update PrimeFactorsForGivenNumber.java
    PrimeFactorsForGivenNumber

--- PrimeFactorsForGivenNumber.java
+++ PrimeFactorsForGivenNumber.java
@@ -3,7 +3,21 @@ class PrimeFactorsForGivenNumber{
     static Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
         int n=sc.nextInt();
+        int fact=0;
         int cama=1;
+
+        for(int j=2; j<=(int)Math.sqrt(n); j++){
+            if(n%j==0){
+                fact++;
+            }
+            if(fact==1){
+                break;
+            }
+        }
+        if(fact==0){
+            System.out.print(n);
+            return;
+        }
         
         for(int i=2; i<=n; i++){
             while(n%i==0){
